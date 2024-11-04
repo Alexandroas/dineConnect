16b7ed52003f1f63b3dfb9e80eea4bcbd26477a5
Alexis Tremeis <alexandroas@hotmail.gr>
2024-11-04 13:37:03 +0200

    Modified to handle the addition of dishes

--- main/utils.py
+++ main/utils.py
@@ -2,32 +2,48 @@
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
-def send_welcome_email(user):
-    context = {
-        'username': user.username,
-        'first_name': user.first_name,
-        'email': user.email,
-    }
+def send_welcome_email(request, user, **kwargs):
+    try:
+        # Check if it's a social login
+        if 'sociallogin' in kwargs:
+            # Get user info from social account
+            social_login = kwargs['sociallogin']
+            email_context = {
+                'username': user.username,
+                'first_name': social_login.account.extra_data.get('given_name', user.username),
+                'email': user.email,
+            }
+        context = {
+            'username': user.username,
+            'first_name': user.first_name,
+            'email': user.email,
+        }
+        html_message = render_to_string('main\email_welcome.html', context)
+        
+        send_mail(
+            subject='Welcome to DineConnect!',
+            message='',
+            html_message=html_message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
+    except Exception as e:
+        print(f"Error sending email: {str(e)}")
+        return False
     
-    html_message = render_to_string('main\email_welcome.html', context)
-    
-    send_mail(
-        subject='Welcome to DineConnect!',
-        message='',
-        html_message=html_message,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
-        fail_silently=False,
-    )
 def send_reservation_email(user, reservation):
     context = {
-        'first_name': user.first_name,
         'user': user,
         'reservation': reservation,
-        'business': reservation.business_id,  # Access the business through reservation
+        'business': reservation.business_id,
+        'dishes' : reservation.dish_id.all(),
+        'debug': True
     }
-    
-    html_message = render_to_string('main\email_reservation.html', context)
+    print(f"Sending email for reservation {reservation.reservation_id}")
+    print(f"Number of dishes: {reservation.dish_id.count()}")
+    print(f"Dishes: {list(reservation.dish_id.all())}")
+    html_message = render_to_string('main/email_reservation.html', context)
     
     try:
         send_mail(
