95f92edb764e4bb9c0b6042dc7aece3e24422908
Alexis Tremeis <alexandroas@hotmail.gr>
2024-11-08 20:27:47 +0200

    Changed date and time input

--- Restaurant_handling/forms.py
+++ Restaurant_handling/forms.py
@@ -38,6 +38,24 @@ class DishUpdateForm(forms.ModelForm):
     def clean_is_available(self):
         return self.cleaned_data['is_available'] == 'True'
 class ReservationForm(forms.ModelForm):
+    reservation_date = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+                'class': 'dateinput'
+            }
+        )
+    )
+    
+    reservation_time = forms.TimeField(
+        widget=forms.TimeInput(
+            attrs={
+                'type': 'time',
+                'class': 'timeinput'
+            }
+        )
+    )
+
     class Meta:
         model = Reservation
         fields = ['reservation_date', 'reservation_time', 'reservation_party_size', 'reservation_special_requests']
