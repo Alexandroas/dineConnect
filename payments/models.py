44db5aab66faef4d4f9870500367cafede5952ec
Alexis Tremeis <alexandroas@hotmail.gr>
2024-11-15 01:32:58 +0200

    Fixed some relationships

--- payments/models.py
+++ payments/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from reservations.models import Reservation
+from django.conf import settings
 # Create your models here.
 class Payment(models.Model):
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    reservation = models.ForeignKey('reservations.Reservation', on_delete=models.CASCADE, related_name='payments')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     stripe_payment_intent_id = models.CharField(max_length=200)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='payments')
+    business = models.ForeignKey('gfgauth.Business', on_delete=models.CASCADE, related_name='payments')
     status = models.CharField(max_length=100, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
