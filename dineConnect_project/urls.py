0c1b404052138d3aff06d788f4984de5247567b6
Alexis Tremeis <alexandroas@hotmail.gr>
2024-12-13 13:46:49 +0200

    Added urls for custom allauth templates

--- dineConnect_project/urls.py
+++ dineConnect_project/urls.py
@@ -18,11 +18,37 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from dineConnect_project import settings
-
+from django.contrib.auth import views as auth_views
+from gfgauth.forms import CustomPasswordResetForm
+from django.urls import reverse_lazy
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gfgauth.urls')),
     path('main/', include('main.urls')),
+    path('accounts/password/reset/',
+    auth_views.PasswordResetView.as_view(
+        template_name='gfgauth/password_reset_form.html',  # Make sure this matches your file structure
+        email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt',
+        success_url='/accounts/password/reset/done/',
+        form_class=CustomPasswordResetForm,
+    ),
+    name='password_reset'),
+    path('accounts/password/reset/done/', 
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='gfgauth/password_reset_done.html'
+        ), 
+        name='password_reset_done'),
+    path('accounts/password/reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'
+        ), 
+        name='password_reset_confirm'),
+    path('accounts/password/reset/complete/', 
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ), 
+        name='password_reset_complete'),
     path('accounts/', include('allauth.urls')),
     path('notifications/', include('notifications.urls')),
     path('restaurant_handling/', include('Restaurant_handling.urls')),
@@ -30,6 +56,7 @@ urlpatterns = [
     path('payments/', include('payments.urls')),
     path('reservations/', include('reservations.urls')),
     path('graphs/', include('graphs.urls')),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
